3ca6239b88821a5b5995ffac026af7b5e272925f
Jongwon Jung <104818841+LENCEOO@users.noreply.github.com>
2025-05-26 17:52:42 +0900

    Timer Implementation

--- Assets/Scripts/UI/Quit.cs
+++ Assets/Scripts/UI/Quit.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-namespace IG.UI 
+namespace IG.UI
 {
     public class Quit : MonoBehaviour
     {
-        public void QuitGame() 
+        public void QuitAndReset()
         {
+            // 저장된 모든 데이터 삭제
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.SetInt("CurrentLevel", 1);
+            PlayerPrefs.SetInt("LastUnlockedLevel", 1);
+            PlayerPrefs.Save();
+
+#if UNITY_EDITOR
+            // 에디터에서는 Play 모드 종료
+            EditorApplication.isPlaying = false;
+#else
+            // 빌드된 게임에서는 앱 종료
             Application.Quit();
+#endif
         }
     }
 }
